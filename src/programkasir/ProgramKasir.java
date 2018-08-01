f2086a9a90e90b3c963b8de4d667e2e0a370601f
ACER E15 <ACER E15@DESKTOP-FP9P8UI>
2018-08-01 14:44:32 +0700

    Initial Commit

--- src/programkasir/ProgramKasir.java
+++ src/programkasir/ProgramKasir.java
@@ -18,6 +18,34 @@ public class ProgramKasir {
      int diskon = 10;
      Scanner masuk = new Scanner(System.in);
      
-     
+     do{ 
+            System.out.println("\n Selamat Datang");
+            System.out.print("Masukan Nama : ");
+            nama = masuk.next();
+            System.out.print("Jumlah barang : ");
+            jumlah = masuk.nextInt();
+            System.out.print("Harga barang : Rp. ");
+            harga = masuk.nextInt();
+            System.out.print("Punya kartu anggota? (y=1, n=2)" );
+            anggota = masuk.nextInt();
+            total = jumlah * harga;
+            
+            if (total > 1000000) {
+                diskonA = total / diskon;
+                if (anggota == 1){
+                    diskonB = total / diskon;
+                    totalA = total - diskonA - diskonB;
+                System.out.println("Total : Rp. " + totalA);
+            }else
+                totalA = total - diskonA;
+            System.out.println("Total : Rp. " + totalA);
+            
+        }else 
+                System.out.println("Total : Rp.  " + total);
+                System.out.print(nama + ",   Ingin Melakukan Transaksi Lagi? (y=1, n=2)");
+                kembali = masuk.nextInt();
+                
+        }while (kembali == 1);
+        System.out.print("\n Terima Kasih!");
         }
  }
